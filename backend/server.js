f0fad5eae0167ef94baec35767f166d6cfc652e7
“Kruthikmanubolu” <kruthikmanubolu@gmail.com>
2025-02-26 18:30:08 -0800

    Maybe IDK

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
+  
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/expenses', require('./routes/expenses'));
